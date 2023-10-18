b96c79e982adf80331d35f074403dd32513b375c
Kup96 <philatov96@gmail.com>
2023-10-18 11:59:30 +0300

    add more documentation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -8,10 +8,11 @@ import {
   Res,
   Param,
   Patch,
+  HttpStatus,
 } from '@nestjs/common';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
 import { UserService } from '../services/user.service';
-import { ApiTags } from '@nestjs/swagger';
+import {ApiBearerAuth, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from '../guards/roles.guard';
 import { Roles } from '../decorators/roles.decorator';
 import { AccessTokenDto, UserRoleDto } from '../dto/UserDto.dto';
@@ -23,10 +24,23 @@ export class UserController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiOperation({ summary: "Login to account" })
+  @ApiQuery({ name: "login", required: true})
+  @ApiQuery({ name: "password", required: true })
+  @ApiResponse({ status: HttpStatus.OK, description: "Success"})
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Unauthorized" })
   async login(@Request() request, @Body() body) {
     return await this.userService.login(body);
   }
 
+  @ApiOperation({ summary: "Register account" })
+  @ApiQuery({ name: "login", required: true})
+  @ApiQuery({ name: "password", required: true })
+  @ApiQuery({ name: "role" })
+  @ApiResponse({ status: HttpStatus.OK, description: "Success"})
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Unauthorized" })
   @Post('register')
   async register(@Request() request, @Body() body) {
     return await this.userService.register(body);
@@ -43,13 +57,25 @@ export class UserController {
     return user;
   }
 
+  @ApiOperation({ summary: "Get users by role" })
+  @ApiResponse({ status: HttpStatus.OK, description: "Success"})
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Unauthorized" })
   @Roles(UserRoleDto.USER, UserRoleDto.ADMINISTRATOR, UserRoleDto.BOSS)
   @UseGuards(RolesGuard)
   @Get('allusers')
+  @ApiBearerAuth('JWT-auth')
   async getAllUsers(@Res() response, @Request() request) {
     return await this.userService.getAllUsers(request.user);
   }
 
+  @ApiOperation({ summary: "change boss in user" })
+  @ApiParam({ name: "id", required: true, description: "user id which needed for new boss" })
+  @ApiQuery({ name: "newBossLogin", required: true, description: "login of new boss"})
+  @ApiResponse({ status: HttpStatus.OK, description: "Success"})
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+  @ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: "Unauthorized" })
+  @ApiBearerAuth('JWT-auth')
   @Roles(UserRoleDto.BOSS)
   @UseGuards(RolesGuard)
   @Patch('changeboss/:id')
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,17 @@ async function bootstrap() {
     .setTitle('employers-system')
     .setDescription('REST api')
     .setVersion('1.0.0')
+    .addBearerAuth(
+        {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          name: 'JWT',
+          description: 'Enter JWT token',
+          in: 'header',
+        },
+        'JWT-auth',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
